5fc9dfefd22a70572d5c8b7723e89ef72321c55a
mahmoud.mahany2012@gmail.com <GEThup2017getHUP>
2019-01-27 15:53:32 +0200

    First

---
+++ QuestionSystem/QuestionSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuestionSystem.Startup))]
+namespace QuestionSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
